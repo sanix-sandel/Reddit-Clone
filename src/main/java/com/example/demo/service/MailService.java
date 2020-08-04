0f7a5f5a232e3509cee8d9116d8e43f0591379bd
sanix sandel <sanicksikani@gmail.com>
2020-08-04 15:33:32 +0300

    send email asynchronous

--- src/main/java/com/example/demo/service/MailService.java
+++ src/main/java/com/example/demo/service/MailService.java
@@ -9,6 +9,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +20,8 @@ public class MailService {
     private final JavaMailSender mailSender;
     private final MailContentBuilder mailContentBuilder;
 
+
+    @Async
     public void sendMail(NotificationEmail notificationEmail){
         MimeMessagePreparator messagePreparator= mimeMessage->{
             MimeMessageHelper messageHelper=new MimeMessageHelper(mimeMessage);
