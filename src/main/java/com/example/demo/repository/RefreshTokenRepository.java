a7e1490384f98af50f51fb4cabfc181c2b030f94
sanix <sanicksikani@gmail.com>
2021-01-06 02:56:22 +0300

    services ok

---
+++ src/main/java/com/example/demo/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
+}
